b7964c5366920c2e18a5b9a48c35733a591a2f5a
notmuchhayden <119303558+notmuchhayden@users.noreply.github.com>
2022-12-05 20:04:24 +0900

    {1,2,3} to "\u0001\u0002\u0003" 으로 변환하는 코드 추가

--- src/main/java/org/example/starter/Main.java
+++ src/main/java/org/example/starter/Main.java
@@ -1,8 +1,28 @@
 package org.example.starter;
 
-public class Main {
-    public static void main(String[] args) {
+// import ANTLR's runtime libraries
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
 
-        System.out.println("Hello world! I'm Cd.");
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        // create a CharStream that reads from standard input
+        CharStream input = CharStreams.fromStream(System.in);
+        //ANTLRInputStream input = new ANTLRInputStream(System.in);
+        // create a lexer that feeds off of input CharStream
+        ArrayInitLexer lexer = new ArrayInitLexer(input);
+        // create a buffer of tokens pulled from the lexer
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        // create a parser that feeds off the tokens buffer
+        ArrayInitParser parser = new ArrayInitParser(tokens);
+        ParseTree tree = parser.init(); // begin parsing at init rule
+        //System.out.println(tree.toStringTree(parser));
+
+        // Create a generic parse tree walker that can trigger callbacks
+        ParseTreeWalker walker = new ParseTreeWalker();
+        // Walk the tree created during the parse, trigger callbacks
+        walker.walk(new ShortToUnicodeString(), tree);
+        System.out.println(); // print a \n after translation
     }
 }
